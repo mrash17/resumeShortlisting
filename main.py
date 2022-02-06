0f03e8bc00f94f9401e819da65d255ae1d2589d4
PreetiHarjani <71420942+preeti1382@users.noreply.github.com>
2022-02-06 23:39:47 +0530

    logo updated

--- main.py
+++ main.py
@@ -14,5 +14,25 @@ def login():
 def signup():
     return render_template('signup.html')
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route("/job-post")
+def jobpost():
+    return render_template('jobPost.html')
+
+@app.route("/resume1")
+def template():
+    return render_template("template1.html")
+
+@app.route("/resume2")
+def template2():
+    return render_template("template2.html")
+
+@app.route("/resume3")
+def template3():
+    return render_template("template3.html")
+
 if __name__=='__main__':
     app.run(debug=True)
