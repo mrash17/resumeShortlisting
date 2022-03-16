bd81aeeaaad118f9baeb971d5163553367b0168e
mrash17 <77162392+mrash17@users.noreply.github.com>
2022-03-16 20:22:50 +0530

    Database Updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template, request,session, redirect, url_for, flash
+
 import pymysql.cursors
 from werkzeug.utils import secure_filename
 
@@ -77,7 +78,52 @@ def jobpost():
         if request.method == "POST":
             company_name = request.form["cname"]
             check_id = "select id from recruiter where company_name = '"+company_name+"'"
-    return render_template('jobPost.html')
+        return render_template('jobPost.html')
+
+@app.route('/student_resume', methods=['GET','POST'])
+def student_resume():
+    if request.method=="POST":
+        name = request.form["name"]
+        dob = request.form["dob"]
+        email = request.form["email"]
+        number=request.form["number"]
+        address=request.form["address"]
+        title = request.form["title"]
+        soft_skill= request.form["soft-skill"]
+        technical_skill= request.form["technical-skill"]
+        position=request.form["position"]
+        company_name=request.form["company-name"]
+        period_fom=request.form["period-from"]
+        period_to = request.form["period-to"]
+        description = request.form["description"]
+        project1_title=request.form["project1-title"]
+        project1=request.form["project1"]
+        project1_tech=request.form["project1-tech"]
+        project2_title=request.form["project2-title"]
+        project2=request.form["project2"]
+        project2_tech=request.form["project2-tech"]
+        education=request.form["education"]
+        language=request.form["language"]
+        sql = "INSERT INTO student_resume(name, dob, email ,number ,address ,title ,soft-skill ,technical-skill ,position ,company-name ,period-from ,period-to ,description ,project1-title ,project1 ,project1-tech ,project2-title ,project2 ,project2-tech ,education,language ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        val = (name, dob, email ,number ,address ,title ,soft_skill ,technical_skill ,position ,company_name ,period_fom ,period_to ,description ,project1_title ,project1 ,project1_tech ,project2_title ,project2 ,project2_tech ,education,language)
+    return render_template('student_resume.html')
+
+@app.route('/job_post', methods=['GET','POST'])
+def job_post():
+    if request.method=="POST":
+        cname = request.form["cname"]
+        columns = request.form.getlist('skill[]')
+        skill_query=[]
+        for x in columns:
+            skill = request.form["skill[]"]
+            skill_query.append(getattr(skill, x))
+        
+        experience = request.form["experience"]
+        education=request.form["education"]
+        city=request.form["city"]
+        sql = "INSERT INTO student_resume(cname ,skill_query ,experience ,education ,city ) VALUES (%s,%s,%s,%s,%s)"
+        val = (cname ,skill_query ,experience ,education ,city)
+    return render_template('dashboard.html')
 
 @app.route("/student-resume")
 def studentresume():
